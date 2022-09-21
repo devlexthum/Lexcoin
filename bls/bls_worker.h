f26c25ebcfc16ed1713f507dd8ccf227ae1de730
devlexthum <114077037+devlexthum@users.noreply.github.com>
2022-09-21 20:08:38 +0530

    lex arpan

--- bls/bls_worker.h
+++ bls/bls_worker.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2022 The Lex Core developers
+// Copyright (c) 2020-2022 The Lex Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 

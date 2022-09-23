31a012e428b8b20da8a2771e43d762a0d2304a2c
devlexthum <114077037+devlexthum@users.noreply.github.com>
2022-09-23 13:27:00 +0530

    lexcoin core dev

---
+++ lex.tx.cpp
@@ -0,0 +1,7 @@
+[main]
+host = https://www.transifex.com
+
+[lexcoin.qt-translation-2.5x]
+file_filter = src/qt/locale/lexthum_<lang>.xlf
+source_file = src/qt/locale/lexcoin_en.xlf
+source_lang = en

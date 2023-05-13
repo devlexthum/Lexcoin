55478e7bff93a55d6efa4b5ebc7962d2c254b371
devlexthum <114077037+devlexthum@users.noreply.github.com>
2023-05-13 10:38:38 +0530

    Update bls_ies.h

--- bls/bls_ies.h
+++ bls/bls_ies.h
@@ -1,6 +1,5 @@
 // Copyright (c) 2019-2022 The Lex Core developers
 # Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef LEX_CRYPTO_BLS_IES_H
 #define LEX_CRYPTO_BLS_IES_H

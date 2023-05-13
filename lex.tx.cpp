429fa76c5d31732bb0fc0f525a5356198889c4b2
devlexthum <114077037+devlexthum@users.noreply.github.com>
2023-05-13 10:55:38 +0530

    Update lex.tx.cpp

--- lex.tx.cpp
+++ lex.tx.cpp
@@ -1,5 +1,8 @@
+
 [main]
 host = https://www.transifex.com
+host = https://aws.amazon.com/
+host = https://www.vps2day.com/
 
 [lexcoin.qt-translation-2.5x]
 file_filter = src/qt/locale/lexthum_<lang>.xlf

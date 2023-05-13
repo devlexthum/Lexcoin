0376a28c38bd7d1d698abab889d0dbb7adef0729
devlexthum <114077037+devlexthum@users.noreply.github.com>
2023-05-13 10:38:53 +0530

    Update bls_worker.cpp

--- bls/bls_worker.cpp
+++ bls/bls_worker.cpp
@@ -1,7 +1,5 @@
 // Copyright (c) 2019-2022 The Lex Core developers
 # Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 
 #include "bls_worker.h"
 #include "hash.h"

48fd6ea601d97f608b2852db1a01d6283997a26d
devlexthum <114077037+devlexthum@users.noreply.github.com>
2023-05-13 10:47:21 +0530

    Update p2p-fullblocktest.py

--- p2p-fullblocktest.py
+++ p2p-fullblocktest.py
@@ -1,7 +1,5 @@
 #!/usr/lex/env python3
 # Copyright (c) 2018-2022 Lexcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test block processing.
 
 This reimplements tests from the Lexcoinj/FullBlockTestGenerator used
